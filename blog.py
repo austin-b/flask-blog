d8c274fae7891a36a92d31c2bfe61aca7a3754ba
Tom <t.austinbennett@gmail.com>
2020-05-18 16:38:07 -0400

    split templates into layout, home, archive, and about_me. Added navbar links and changed the css.

--- blog.py
+++ blog.py
@@ -15,5 +15,15 @@ def home():
     return render_template('home.html')
 
 
+@app.route('/archive')
+def archive():
+    return render_template('archive.html')
+
+
+@app.route('/about_me')
+def about_me():
+    return render_template('about_me.html')
+
+
 if __name__ == "__main__":
     app.run(host='127.0.0.1', port=5000)
